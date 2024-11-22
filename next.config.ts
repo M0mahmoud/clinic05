630f026ed49730cb457682dd114eda1399af74b3
M0mahmoud <houdmohamed85@gmail.com>
2024-11-22 19:15:38 +0200

    Setup Project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+        ignoreBuildErrors: true, // This will skip TypeScript errors in the build process
+    },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
